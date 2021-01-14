dd033e96c4c744775e0a13ae954bf2681b230542
amjadsam <68984249+amjadsam@users.noreply.github.com>
2021-01-14 23:26:28 +0530

    changed

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,14 @@ import '../styles/App.css';
 
 class App extends Component {
     render() {
-
+     const relatives=["amjad","arshad","aijaz"];
         return(
             <div id="main">
                {/* Do not remove the main div */}
+            <ol key="relativeList">
+                {relatives.map((el,index)=>
+                    <li key={"relativeListItem"+(index+1)}>{el}</li>)}
+                </ol>
             </div>
         )
     }
